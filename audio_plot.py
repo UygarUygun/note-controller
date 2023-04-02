f942e7084c9b1e6993180c6bc3cb15307667666c
UygarUygun <uygaruygun@hotmail.com>
2023-04-02 05:49:37 +0300

    new real time plot
    added real time plot but the first plot requires maintenance, still cant determine the pitch but at least we can now visualize the input signal

--- audio_plot.py
+++ audio_plot.py
@@ -1,78 +1,90 @@
 import pyaudio
+import struct
 import numpy as np
-import math
 import matplotlib.pyplot as plt
+import time
 
-# initialize PyAudio
+# Parameters
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+CHUNK = 1024*4
+PLOT_INTERVAL = 50 # milliseconds
+GAIN = 1.0
+
+# PyAudio object
 p = pyaudio.PyAudio()
 
-# open stream using default input device
-stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=44100,
+# Stream object
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
                 input=True,
-                frames_per_buffer=1024,
+                frames_per_buffer=CHUNK,
                 input_device_index=3)
 
-# create a new plot to show the pitch and signal over time
-fig, (ax_pitch, ax_signal) = plt.subplots(2, figsize=(8, 6))
-x = np.arange(0, 2 * 1024, 2)
-line_pitch, = ax_pitch.plot([], [], lw=2)
-line_signal, = ax_signal.plot([], [], lw=2)
-ax_pitch.set_ylim(0, 1500)
-ax_pitch.set_xlim(0, 1024)
-ax_pitch.set_title('Pitch over time')
-ax_pitch.set_xlabel('Time')
-ax_pitch.set_ylabel('Pitch (Hz)')
-ax_signal.set_ylim(-1, 1)
-ax_signal.set_xlim(0, 2 * 1024)
-ax_signal.set_title('Signal over time')
-ax_signal.set_xlabel('Time')
-ax_signal.set_ylabel('Amplitude')
+# Figure object and subplots
+fig, (ax1, ax2) = plt.subplots(2)
 
-fundamental_freq = 0.0
+# Frequency axis
+freqs = np.fft.rfftfreq(CHUNK, 1/RATE)
 
-# callback function to read audio data and update plot
-def update_plot(frame):
-    global fundamental_freq
+# Line objects
+line_fft, = ax1.plot(freqs, np.zeros(CHUNK//2+1))
+line_signal, = ax2.plot(np.arange(0, CHUNK)/RATE*1000, np.zeros(CHUNK))
 
-    # read audio data
-    data = stream.read(1024, exception_on_overflow=False)
-    # convert data to numpy array
-    data = np.frombuffer(data, dtype=np.float32)
+# Axis settings
+ax1.set_xlim(20, RATE/2)
+ax1.set_ylim(0, 0.05)
+ax2.set_xlim(0, (CHUNK/RATE)*1000)
+ax2.set_ylim(-20000, 20000)
 
-    # calculate pitch
-    autocorr = np.correlate(data, data, mode='full')
-    autocorr = autocorr[len(autocorr)//2:]
-    # find the first peak in the autocorrelation
-    start = 0
-    while autocorr[start] > autocorr[start + 1]:
-        start += 1
-    peak = start
-    while peak < len(autocorr) and autocorr[peak] > autocorr[peak - 1]:
-        peak += 1
-    if peak == len(autocorr):
-        peak = 0
-    fundamental_period = peak + start
-    fundamental_freq = 44100.0 / fundamental_period
-    # handle case where fundamental_freq is zero or negative
-    if fundamental_freq <= 0:
-        note_index = 0
-    else:
-        note_index = int(round(12 * math.log2(fundamental_freq / 440.0))) % 12
+# Plot titles
+ax1.set_title('FFT')
+ax2.set_title('Signal')
 
-    # update plot
-    line_pitch.set_data(x[:len(data)//2], autocorr[:len(data)//2])
-    line_signal.set_data(x, data)
-    ax_pitch.set_title(f'Pitch over time: {note_names[note_index]} ({fundamental_freq:.2f} Hz)')
-    fig.canvas.draw()
-    fig.canvas.flush_events()
+# Set up the gain slider
+def on_gain_change(val):
+    global GAIN
+    GAIN = val
+    print(f'Gain changed to {val}')
 
-    return (line_pitch,)
+gain_slider_ax = fig.add_axes([0.15, 0.1, 0.7, 0.05])
+gain_slider = plt.Slider(gain_slider_ax, 'Gain', 0, 10, valinit=GAIN)
+gain_slider.on_changed(on_gain_change)
 
-note_names = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+# Display figure
+plt.tight_layout()
+plt.ion()
+plt.show()
 
-# start animation
+# Variable to keep track of the last time the plot was updated
+last_plot_time = 0
+
+# Infinite loop
 while True:
-    ani = update_plot(None)
+    # Read data from stream
+    data = stream.read(CHUNK, exception_on_overflow=False)
+    
+    # Convert data to integers
+    data_int = struct.unpack(str(CHUNK) + 'h', data)
+    
+    # Apply gain control
+    data_int = np.array(data_int) * GAIN
+    
+    # Perform FFT and normalize
+    fft = np.fft.rfft(data_int)
+    fft_norm = np.abs(fft) / (CHUNK // 2)
+    
+    # Update line objects
+    line_fft.set_ydata(fft_norm)
+    line_signal.set_ydata(data_int)
+    
+    # Redraw figures
     plt.pause(0.001)
+    
+    # Update plot every PLOT_INTERVAL milliseconds
+    current_time = time.time_ns()
+    if current_time - last_plot_time > PLOT_INTERVAL:
+        last_plot_time = current_time
+        plt.draw()
